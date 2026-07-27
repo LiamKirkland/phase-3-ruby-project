91066cab546ef5e08d9e59714dffad608c3234d4
LiamKirkland <liam.b.kirkland@gmail.com>
2026-07-27 15:41:03 -0400

    Add missing linebreak

--- cli/team_manager.rb
+++ cli/team_manager.rb
@@ -26,7 +26,7 @@ class TeamManager
       team = Team.find_by(id: choice)
 
       if team
-        puts "#{team.name} (ID #{team.id})"
+        puts "\n#{team.name} (ID #{team.id})"
         puts "Record (Win/Lose/Tie): \e[32m#{team.games_won}\e[0m / \e[31m#{team.games_lost}\e[0m / \e[33m#{team.games_tied}\e[0m"
         wr = format("%.1f", (team.games_won.to_f / team.games_played) * 100)
         puts "Games Played (WR%): #{team.games_played} (#{wr}%)"
